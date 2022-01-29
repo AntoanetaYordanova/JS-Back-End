51a3eef6ba14721570cd3f5364ce22c86b197f1a
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-01-29 11:01:40 +0100

    JS Back End

--- 06.Workshop MongoDB and Mongoose/project/controllers/create.js
+++ 06.Workshop MongoDB and Mongoose/project/controllers/create.js
@@ -1,17 +1,23 @@
 module.exports = {
-    create: (req, res) => {
-        res.render('create', { title: 'Create Cube' });
-    },
-    post: async (req, res) => {
-        const cube = {
-            name: req.body.name,
-            description: req.body.description,
-            imageUrl: req.body.imageUrl,
-            difficulty: Number(req.body.difficulty)
-        };
+  create: (req, res) => {
+    res.render('create', { title: 'Create Cube' });
+  },
+  post: async (req, res) => {
+    const cube = {
+      name: req.body.name,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      difficulty: Number(req.body.difficulty),
+    };
 
-        await req.storage.create(cube);
-        
-        res.redirect('/');
+    try {
+      await req.storage.create(cube);
+    } catch (err) {
+      if(err.name == 'Validation error') {
+          return res.render('create', { title: 'Create Cube', error : 'All fields are required' })
+      }
     }
+
+    res.redirect('/');
+  },
 };
--- 06.Workshop MongoDB and Mongoose/project/models/Cube.js
+++ 06.Workshop MongoDB and Mongoose/project/models/Cube.js
@@ -1,10 +1,25 @@
 const {Schema, model} = require('mongoose');
 
 const cubeSchema = new Schema({
-    name : String,
-    description : String,
-    imageUrl : String,
-    difficulty : Number,
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true,
+        maxlength : 500
+    },
+    imageUrl : {
+        type : String,
+        required : true,
+        match : /^https?:\/\//
+    },
+    difficulty : {
+        type : Number,
+        min : 1,
+        max : 6
+    },
 });
 
 module.exports = model('Cube', cubeSchema);
